81dd9d2079580e569514485aaaf56789c1be7823
Bjorn Worrall <85297118+lasity34@users.noreply.github.com>
2024-09-25 14:00:22 +0200

    notification wrappped

--- waiter-app-frontend/src/App.tsx
+++ waiter-app-frontend/src/App.tsx
@@ -5,18 +5,22 @@ import SignIn from './components/Signin';
 import WaiterDashboard from './components/WaiterDashboard';
 import ManagerDashboard from './components/ManagerDashboard';
 import axios from 'axios';
+import { NotificationProvider } from './components/NotificationSystem';
 
 axios.defaults.withCredentials = true;
+
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/waiter-dashboard" element={<WaiterDashboard />} />
-        <Route path="/manager-dashboard" element={<ManagerDashboard />} />
-      </Routes>
-    </div>
+    <NotificationProvider>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/waiter-dashboard" element={<WaiterDashboard />} />
+          <Route path="/manager-dashboard" element={<ManagerDashboard />} />
+        </Routes>
+      </div>
+    </NotificationProvider>
   );
 };
 
